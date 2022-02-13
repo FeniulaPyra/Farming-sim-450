b0852afa08fb7fc5da9862b981c827941d33642d
sadegun93 <43414993+sadegun93@users.noreply.github.com>
2022-02-13 12:05:37 -0500

    Started on time
    Displaying time and date now works just fine, it looks like

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TimeManager : MonoBehaviour
 {
@@ -16,7 +18,25 @@ public class TimeManager : MonoBehaviour
     float dayTimer = daysInSeconds;
 
     //Reference to the FarmManager so it can access the dictionary that has all of the mushrooms
-    public FarmManager management;// = new FarmManager();
+    public FarmManager management;
+
+    //variables for displaying the date and time and their corresponding text object
+    //1-7 for Sun - Sat
+    int dayNum = 1;
+    public TMP_Text dayDisplay;
+    //1 - 30 for season length
+    int dateNum = 1;
+    public TMP_Text dateDisplay;
+    //1 - 4 for Spr - Win
+    int seasonNum = 1;
+    public TMP_Text seasonDisplay;
+    int yearNum = 1;
+    public TMP_Text yearDisplay;
+    int hourCount = 6;
+    public TMP_Text hourDisplay;
+    int minuteCount = 0;
+    public TMP_Text minuteDisplay;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +44,14 @@ public class TimeManager : MonoBehaviour
         // Debug.Log($"There are {daysInMinutes} minutes in the day and {daysInSeconds} seconds in the day");
 
         //Getting Time Manager
-        management = FindObjectOfType<FarmManager>();
+        //management = FindObjectOfType<FarmManager>();
+
+        dayDisplay.text = "Sun";
+        dateDisplay.text = "1";
+        seasonDisplay.text = "Spring";
+        yearDisplay.text = "Year 1";
+
+        DisplayTime();
     }
 
     // Update is called once per frame
@@ -36,9 +63,11 @@ public class TimeManager : MonoBehaviour
             AdvanceDay();
         }
 
+        DisplayTime();
+
         //Also temp code
         //Counts down the day, then when it's over, calls the method
-        dayTimer -= Time.deltaTime;
+        //dayTimer -= Time.deltaTime;
          Debug.Log($"There are {dayTimer} seconds remaining");
 
         if(dayTimer <= 0.00f)
@@ -47,7 +76,9 @@ public class TimeManager : MonoBehaviour
         }
     }
 
-    //Method to change the day and call grow Mushroom
+    /// <summary>
+    /// Method to change the day and call grow Mushroom
+    /// </summary>
     void AdvanceDay()
     {
 
@@ -81,6 +112,152 @@ public class TimeManager : MonoBehaviour
 
         //Once all of the mushrooms grow, call spread once.
         management.SpreadMushroom();
+
+        DisplayDate();
+    }
+
+    public void DisplayTime()
+    {
+        //display hour
+        if(minuteCount == 59)
+        {
+            if (hourCount < 24)
+            {
+                hourCount++;
+            }
+            else
+            {
+                hourCount = 0;
+
+                //Advances the day if the clock passes midnight, which just makes sense
+                AdvanceDay();
+            }
+
+        }
+
+        if(hourCount < 10)
+        {
+            hourDisplay.text = $"0{hourCount}:";
+        }
+        else
+        {
+            hourDisplay.text = $"{hourCount}:";
+        }
+
+        //display minute
+        if (minuteCount == 59)
+        {
+            minuteCount = 0;
+        }
+        else
+        {
+            minuteCount += 1;
+        }
+
+        if (minuteCount < 10)
+        {
+            minuteDisplay.text = $"0{minuteCount}";
+        }
+        else
+        {
+            minuteDisplay.text = minuteCount.ToString(); ;
+        }
+    }
+
+    /// <summary>
+    /// Method that displays the in game date
+    /// </summary>
+    public void DisplayDate()
+    {
+        //change year
+        if (seasonNum == 4 && dateNum == 30)
+        {
+            yearNum++;
+        }
+
+        yearDisplay.text = $"Year {yearNum}";
+
+        //change season
+        if (dateNum == 30)
+        {
+            if (seasonNum == 4)
+            {
+                seasonNum = 1;
+            }
+            else
+            {
+                seasonNum++;
+            }
+        }
+
+        switch (seasonNum)
+        {
+            case 1:
+                seasonDisplay.text = "Spring";
+                break;
+            case 2:
+                seasonDisplay.text = "Summer";
+                break;
+            case 3:
+                seasonDisplay.text = "Fall";
+                break;
+            case 4:
+                seasonDisplay.text = "Winter";
+                break;
+            default:
+                seasonDisplay.text = $"";
+                break;
+        }
+
+        //change the date
+        if (dateNum == 30)
+        {
+            dateNum = 1;
+        }
+        else
+        {
+            dateNum++;
+        }
+
+        dateDisplay.text = dateNum.ToString();
+
+        //change the day
+        if (dayNum == 7)
+        {
+            dayNum = 1;
+        }
+        else
+        {
+            dayNum++;
+        }
+
+        switch (dayNum)
+        {
+            case 1:
+                dayDisplay.text = "Sun";
+                break;
+            case 2:
+                dayDisplay.text = "Mon";
+                break;
+            case 3:
+                dayDisplay.text = "Tues";
+                break;
+            case 4:
+                dayDisplay.text = "Wed";
+                break;
+            case 5:
+                dayDisplay.text = "Thurs";
+                break;
+            case 6:
+                dayDisplay.text = "Fri";
+                break;
+            case 7:
+                dayDisplay.text = "Sat";
+                break;
+            default:
+                dayDisplay.text = "";
+                break;
+        }
 
         dayTimer = daysInSeconds;
     }
