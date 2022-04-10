43bf1eb7439b1ddb61d50717fc0f59c132301db2
Lauren Puglise <feniuldragon@gmail.com>
2022-04-09 23:52:49 -0400

    added bubble popups denoting interaction range and several bubble sprites

--- Assets/Scripts/Player/InteractableObjects.cs
+++ Assets/Scripts/Player/InteractableObjects.cs
@@ -6,16 +6,33 @@ public class InteractableObjects : MonoBehaviour
 {
     //The name of the object that can be interacted with. If it has this script, it is assumed the gameObject can be interacted with 
     public string name;
+	private GameObject player;
+	private const float DIST_TO_INTERACT = 1.0f;
+	private SpriteRenderer bubbleSR;
+	public Sprite interactBubble;
+	private GameObject bubble;
 
-    // Start is called before the first frame update
-    void Start()
+	// Start is called before the first frame update
+	void Start()
     {
-        
+		player = GameObject.Find("Player");
+		bubble = Instantiate(new GameObject(), gameObject.transform);
+		bubbleSR = bubble.AddComponent<SpriteRenderer>();
+		bubbleSR.sprite = interactBubble;
+		bubble.transform.position = (Vector2)bubble.transform.position + new Vector2(0, .5f);
+		bubble.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		if (Vector2.Distance(gameObject.transform.position, player.transform.position) <= DIST_TO_INTERACT)
+		{
+			bubble.SetActive(true);
+		}
+		else
+		{
+			bubble.SetActive(false);
+		}
     }
 }
