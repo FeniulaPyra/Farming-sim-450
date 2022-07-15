894134ad401d083333213118738faa15c9043828
sadegun93 <43414993+sadegun93@users.noreply.github.com>
2022-07-15 12:49:34 -0400

    Combat Test Scene for Build
    Enemy Spawn Test Scene slightly altered to make it more visually distinct from the Ground Scene to avoid confusion for testers.
    
    New tile palettes created.

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -468,32 +468,35 @@ public class PlayerInteraction : MonoBehaviour
                 heldItemAmount = playerInventoryManager.GetHeldItemAmount();
 
                 //Debug.Log("LEP2738 HELD: " + heldItem.name);
-                if (mushroomsAndTiles.ContainsKey(focusTilePosition))
+                if (farmManager.farmField != null && farmManager.tillableGround != null)
                 {
-                    if (heldItemAmount > 0 && heldItem.type=="mushroom" && mushroomsAndTiles[focusTilePosition].isTilled == true && mushroomsAndTiles[focusTilePosition].hasPlant == false)//if(farmManager.GetComponent<FarmManager>().playerInventory.HeldItem.Amount > 0 && itemName.Contains("Shroom"))
+                    if (mushroomsAndTiles.ContainsKey(focusTilePosition))
                     {
-                        Debug.Log("Plant One");
-                        playerInventoryManager.RemoveHeldItems(1);
-                    }
+                        if (heldItemAmount > 0 && heldItem.type == "mushroom" && mushroomsAndTiles[focusTilePosition].isTilled == true && mushroomsAndTiles[focusTilePosition].hasPlant == false)//if(farmManager.GetComponent<FarmManager>().playerInventory.HeldItem.Amount > 0 && itemName.Contains("Shroom"))
+                        {
+                            Debug.Log("Plant One");
+                            playerInventoryManager.RemoveHeldItems(1);
+                        }
 
 
-                    //before actually doing interaction, deduct player stamina accordingly
-                    //switch on the four main item types, then some default value for everything else
-                    if (heldItem.type == "mushroom" && mushroomsAndTiles[focusTilePosition].isTilled == true && mushroomsAndTiles[focusTilePosition].hasPlant == false)
-                    {
-                        ReduceStamina(heldItem.staminaUsed);
-                    }
-                    else if (itemName == "Sickle" && mushroomsAndTiles[focusTilePosition].hasPlant == true)
-                    {
-                        ReduceStamina(heldItem.staminaUsed);
-                    }
-                    else if (itemName == "Watering Can" && mushroomsAndTiles[focusTilePosition].isTilled == true && mushroomsAndTiles[focusTilePosition].isMoist == false)
-                    {
-                        ReduceStamina(heldItem.staminaUsed);
-                    }
-                    else if (itemName == "Hoe" && mushroomsAndTiles[focusTilePosition].isTilled == false)
-                    {
-                        ReduceStamina(heldItem.staminaUsed);
+                        //before actually doing interaction, deduct player stamina accordingly
+                        //switch on the four main item types, then some default value for everything else
+                        if (heldItem.type == "mushroom" && mushroomsAndTiles[focusTilePosition].isTilled == true && mushroomsAndTiles[focusTilePosition].hasPlant == false)
+                        {
+                            ReduceStamina(heldItem.staminaUsed);
+                        }
+                        else if (itemName == "Sickle" && mushroomsAndTiles[focusTilePosition].hasPlant == true)
+                        {
+                            ReduceStamina(heldItem.staminaUsed);
+                        }
+                        else if (itemName == "Watering Can" && mushroomsAndTiles[focusTilePosition].isTilled == true && mushroomsAndTiles[focusTilePosition].isMoist == false)
+                        {
+                            ReduceStamina(heldItem.staminaUsed);
+                        }
+                        else if (itemName == "Hoe" && mushroomsAndTiles[focusTilePosition].isTilled == false)
+                        {
+                            ReduceStamina(heldItem.staminaUsed);
+                        }
                     }
                 }
 
